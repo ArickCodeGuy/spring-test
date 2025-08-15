038b781086400fc83d775ffc1b7af9cbaf7887f1
ArickCodeGuy <arickmail1997@gmail.com>
2025-08-15 06:12:59 +0300

    oauth2

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/public/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+            )
+            .logout(logout -> logout
+                .permitAll())
+            // @@TODO too lazy to fix
+            .oauth2Login();
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore/**");
+    }
+}
